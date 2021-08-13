9b2b61323ddfdb1598670d5f04593741ceea6885
JackGardnerRMIT <80367337+JackGardnerRMIT@users.noreply.github.com>
2021-08-13 12:35:34 +1000

    Add files via upload

---
+++ number_guesser.py
@@ -0,0 +1,37 @@
+import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number larger then 0 next time.")
+        quit()
+else:
+    print("Please type a number next time.")
+    quit()
+
+print("Try picking numbers between 0 and %s" % (top_of_range))
+
+random_number = random.randint(0, top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make as guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+
+    if user_guess == random_number:
+        print("You got it correct!")
+        break 
+    elif user_guess > random_number:
+        print("You were above the number!")
+    else:
+        print("You were below the number!")
+
+print("You got it in %s guesses" % (guesses))
